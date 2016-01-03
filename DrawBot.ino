044960c160b0ff81ea79cc7438a277711f25f7c6
Karlduino <karlduino@gmail.com>
2016-01-03 15:13:31 -0600

    Re-do sketch to make it more interesting

--- DrawBot.ino
+++ DrawBot.ino
@@ -8,7 +8,7 @@
 #include <Adafruit_MCP23017.h>
 #include <BricktronicsShield.h>
 
-#define MAXSPEED 150
+#define MAXSPEED 200
 #define MINSPEED 60
 
 BricktronicsMotor leftmotor(BricktronicsShield::MOTOR_1);
@@ -19,7 +19,7 @@ BricktronicsButton button(BricktronicsShield::SENSOR_2);
 
 void setup()
 {
-  BricktronicsShield::begin();  
+  BricktronicsShield::begin();
   leftmotor.begin();
   rightmotor.begin();
   button.begin();
@@ -28,48 +28,84 @@ void setup()
   randomSeed(analogRead(A0));
 }
 
-long timeout = -1;
+int time=2000;
+int speed=MAXSPEED/2;
+int turn_time = 2500;
 bool prev_button_pressed = false;
 bool button_pressed;
 
 void loop()
 {
-  if(timeout >= 0 && timeout < millis())
-  {
-    timeout_expired();
-  }
+  speed += random(-50, 50);
+  if(speed > MAXSPEED) speed = MAXSPEED;
+  if(speed < MINSPEED) speed = MINSPEED;
 
-  button_pressed = button.isPressed();
-  if(button_pressed && button_pressed != prev_button_pressed) {
-    bumpers_hit();
-  }
-  prev_button_pressed = button_pressed;
+  forward(random(50, 500), speed);
+
+  if(button.isPressed()) bumpers_hit();
+
+  time += random(-400, 400);
+  if(time < 0) time = 500;
+  left(time, speed);
+
+  if(button.isPressed()) bumpers_hit();
+
+  forward(random(50, 500), speed);
+
+  if(button.isPressed()) bumpers_hit();
+
+  time += random(-400, 400);
+  if(time < 0) time = 500;
+  right(time, speed);
+
+  if(button.isPressed()) bumpers_hit();
+
 }
 
 void bumpers_hit()
 {
-  int right_speed = rightmotor.getFixedDrive() * -0.9;
-  int left_speed = leftmotor.getFixedDrive() * -0.9;
-
-  rightmotor.setFixedDrive(right_speed);
-  leftmotor.setFixedDrive(left_speed);
-
-  delay(500);
-  if(abs(left_speed) < MINSPEED && abs(right_speed) < MINSPEED) {
-    timeout_expired(); // gotten too slow; reset
-  }
+  backward(1000, MAXSPEED*0.8);
+  if(random(0, 1) < 0.5)
+    right(turn_time, MAXSPEED*0.8);
+  else
+    left(turn_time, MAXSPEED*0.8);
 }
 
-void timeout_expired()
+void stop()
 {
-  int right_speed = 0;
-  int left_speed = 0;
-  while(abs(left_speed) < MINSPEED && abs(right_speed) < MINSPEED) {
-    left_speed = random(-MAXSPEED, MAXSPEED);
-    right_speed = random(-MAXSPEED, MAXSPEED);
-  }
-  rightmotor.setFixedDrive(right_speed);
-  leftmotor.setFixedDrive(left_speed);
-  timeout = millis() + random(1,10)*1000;
+  rightmotor.setFixedDrive(0);
+  leftmotor.setFixedDrive(0);
 }
 
+void backward(int amount, int speed)
+{
+  rightmotor.setFixedDrive(speed);
+  leftmotor.setFixedDrive(speed);
+  delay(amount);
+  stop();
+}
+
+void forward(int amount, int speed)
+{
+  rightmotor.setFixedDrive(-speed);
+  leftmotor.setFixedDrive(-speed);
+  delay(amount);
+  stop();
+}
+
+
+void right(int amount, int speed)
+{
+  rightmotor.setFixedDrive(speed);
+  leftmotor.setFixedDrive(-speed);
+  delay(amount);
+  stop();
+}
+
+void left(int amount, int speed)
+{
+  rightmotor.setFixedDrive(-speed);
+  leftmotor.setFixedDrive(speed);
+  delay(amount);
+  stop();
+}
