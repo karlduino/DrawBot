35779d5e9dc67a6d6ed9ac3eed24dce677aa39fd
Karlduino <karlduino@gmail.com>
2016-01-05 15:19:47 -0600

    Trying to make a geometric design; not pretty, still

--- DrawBot.ino
+++ DrawBot.ino
@@ -16,37 +16,13 @@ BricktronicsMotor rightmotor(BricktronicsShield::MOTOR_2);
 
 BricktronicsButton button(BricktronicsShield::SENSOR_2);
 
-
-void setup()
-{
-  BricktronicsShield::begin();
-  leftmotor.begin();
-  rightmotor.begin();
-  button.begin();
-
-  // initialize random number generator
-  randomSeed(analogRead(A0));
-}
-
-int time=200;
-int speed=150;
-int turn_time = 25;
+int time1 = 300, time2 = 40;
+int lspeed1=100, rspeed1 = 140;
+int lspeed2=-140, rspeed2 = 160;
+int accel_steps = 20;
+int accel_time = 1;
 bool prev_button_pressed = false;
 bool button_pressed;
-
-void loop()
-{
-  speed = limit_value(speed + random(-50, 50), MINSPEED, MAXSPEED);
-  forward(random(5, 50), speed);
-
-  time = limit_value(time + random(-40, 40), 50, 500);
-  left(time, speed);
-
-  forward(random(5, 50), speed);
-
-  time = limit_value(time + random(-40, 40), 50, 500);
-  right(time, speed);
-}
 
 void bumpers_hit()
 {
@@ -70,38 +46,32 @@ void stop()
   leftmotor.setFixedDrive(0);
 }
 
-void backward(int amount, int speed)
+// drive at a fixed speed for a given length of time
+void drive(int left_speed, int right_speed,
+           int travel_time)
 {
-  rightmotor.setFixedDrive(speed);
-  leftmotor.setFixedDrive(speed);
-  look_for_button(amount);
-  stop();
+    leftmotor.setFixedDrive(left_speed);
+    rightmotor.setFixedDrive(right_speed);
+    look_for_button(travel_time);
 }
 
-void forward(int amount, int speed)
+// accelerate from one speed to another in a fixed number of steps
+void accel(int left_start, int right_start,
+           int left_end, int right_end,
+           int time_increment, int steps)
 {
-  rightmotor.setFixedDrive(-speed);
-  leftmotor.setFixedDrive(-speed);
-  look_for_button(amount);
-  stop();
+    float left_step = (float)(left_end - left_start)/(float)steps;
+    float right_step = (float)(right_end - right_start)/(float)steps;
+
+    for(int i=0; i<steps; i++) {
+        leftmotor.setFixedDrive(i*left_step + left_start);
+        rightmotor.setFixedDrive(i*right_step + right_start);
+        look_for_button(time_increment);
+    }
+    leftmotor.setFixedDrive(left_end);
+    rightmotor.setFixedDrive(right_end);
 }
 
-
-void right(int amount, int speed)
-{
-  rightmotor.setFixedDrive(speed);
-  leftmotor.setFixedDrive(-speed);
-  look_for_button(amount);
-  stop();
-}
-
-void left(int amount, int speed)
-{
-  rightmotor.setFixedDrive(-speed);
-  leftmotor.setFixedDrive(speed);
-  look_for_button(amount);
-  stop();
-}
 
 void look_for_button(int amount)
 {
@@ -114,10 +84,43 @@ void look_for_button(int amount)
   }
 }
 
+
 int limit_value(int number, int low, int high)
 {
   if(number < low) return low;
   if(number > high) return high;
   return number;
 }
+
+void setup()
+{
+    BricktronicsShield::begin();
+    leftmotor.begin();
+    rightmotor.begin();
+    button.begin();
+
+    // initialize random number generator
+    randomSeed(analogRead(A0));
+
+    // accel to 1st speed
+    accel(0, 0, lspeed1, rspeed1, accel_time, accel_steps);
+}
+
+
+void loop()
+{
+    // drive at 1st speed
+    drive(lspeed1, rspeed1, time1);
+
+    // accel to 2nd speed
+    accel(lspeed1, rspeed1, lspeed2, rspeed2, accel_time, accel_steps);
+
+    // drive at 2nd speed
+    drive(lspeed2, rspeed2, time2);
+
+    // accel back to 1st speed
+    accel(lspeed2, rspeed2, lspeed1, rspeed1, accel_time, accel_steps);
+}
+
+
 
